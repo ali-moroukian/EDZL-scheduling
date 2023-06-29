3f21d6c2855e4532972efdcaf790840018fba277
moroukian <moroukianali@gmail.com>
2023-06-30 03:07:39 +0330

    fix(matplotlib.colors): Increase number of colors to 16 = max number of cores

--- config.py
+++ config.py
@@ -4,3 +4,22 @@ N = 10  # No. of Tasks
 M = random.choice([4, 8, 16])  # No. of Cores
 
 PERIODS = [10, 20, 100]
+
+MATPLOTLIB_COLORS = [
+    'tab:blue',
+    'tab:orange',
+    'tab:green',
+    'tab:red',
+    'tab:purple',
+    'tab:brown',
+    'tab:pink',
+    'tab:gray',
+    'tab:olive',
+    'tab:cyan',
+    'black',
+    'chocolate',
+    'gold',
+    'lime',
+    'navy',
+    'silver',
+]
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
 from collections import defaultdict
 
-import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 
 from config import *
@@ -157,7 +156,7 @@ def visualize(json_object):
             ax.broken_barh(
                 [[i[0], i[1] - i[0]]],
                 [5 * task_id, 4],
-                facecolors=list(mcolors.TABLEAU_COLORS.keys())[i[2]]
+                facecolors=MATPLOTLIB_COLORS[i[2]]
             )
     ax.set_yticks([i * 5 + 2 for i in range(len(task_ids))])
     ax.set_yticklabels([f"Task {id_}" for id_ in task_ids])
